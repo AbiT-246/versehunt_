bc7cd8319e2c6704eacbaaf1b6420c912de16fd7
Abigia Debebe <Abitibebu123@gmail.com>
2023-06-06 01:31:27 +0300

    Text to speech working

--- src/Pages/Poem.js
+++ src/Pages/Poem.js
@@ -1,13 +1,13 @@
 import React, { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
 import "./Poem.css";
-import axios from "axios";
+import { useSpeechSynthesis } from "react-speech-kit";
 
 function Poem() {
   const { title } = useParams();
   const [lines, setLines] = useState([]);
-  const [author, setAuthor] = useState("");
-  const [authorInfo, setAuthorInfo] = useState({});
+  const [isPlaying, setIsPlaying] = useState(false); // State variable to track if speech is playing
+  const { speak, cancel } = useSpeechSynthesis();
 
   useEffect(() => {
     const fetchLines = async () => {
@@ -16,7 +16,6 @@ function Poem() {
         const response = await fetch(currUrl);
         const data = await response.json();
         setLines(data[0].lines);
-        setAuthor(data[0].author);
       } catch (error) {
         console.error("Error:", error);
       }
@@ -52,17 +51,34 @@ function Poem() {
   //   fetchAuthor();
   // }, []);
 
-  console.log(lines);
+  function textToSpeech() {
+    if (!isPlaying) {
+      const poemText = lines.join(" ");
+      speak({ text: poemText });
+      setIsPlaying(true);
+    } else {
+      cancel();
+      setIsPlaying(false);
+    }
+  }
 
   return (
     <div className="Holder">
-      <span className="som">
-        <i className="fas fa-star fa-2x"></i>
-      </span>
       <h2>{title}</h2>
-      {lines.map((line) => {
-        return <p id="lines">{line}</p>;
-      })}
+
+      <span onClick={textToSpeech} className="icon1 m-3">
+        <i className="fas fa-microphone" style={{ fontSize: "150%" }}></i>
+      </span>
+      <span className="icon2 m-3">
+        <i className="fas fa-language" style={{ fontSize: "150%" }}></i>
+      </span>
+
+      {lines.map((line, index) => (
+        <p key={index} id="lines">
+          {line}
+        </p>
+      ))}
+
       <div id="author" className="position-absolute d-inline-block">
         HELLO
       </div>
